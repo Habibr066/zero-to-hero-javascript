51bd4ad8f74b1f6ebf41b05f753d0c195aae53ed
Habib Rehman <99414376+Habibr066@users.noreply.github.com>
2023-12-30 12:44:21 +0000

    g

--- basic/string.js
+++ basic/string.js
@@ -1,7 +1,7 @@
 const name = "habib urrehman"
 const repoCount = 50
 
-console.log('Hi, My name is ${name} and my repo count is ${repoCount}');
+//onsole.log('Hi, My name is ${name} and my repo count is ${repoCount}');
 
 const gameName = new String('shayan ali shah')
-console.log('gamename ${game}')
+
