078331184a58c98bd942dd52460a8a106ce027ff
Habib Rehman <99414376+Habibr066@users.noreply.github.com>
2023-12-30 13:56:08 +0000

    json to node

--- basic/string.js
+++ basic/string.js
@@ -1,8 +1,19 @@
 const mName = 'habib urrehman'
 const repo = 50
 
-console.log(mName + ' repo is '+ repo)
+//console.log(mName + repo)
 
-//const gameName = new String('shayan ali shah')
+const gameName = new String('shayan ali shah')
 
 
+/** string methods to toUpperCase */
+
+//console.log(gameName.toUpperCase())
+//console.log(mName.toUpperCase());
+
+//search the items through index[0,1]
+//console.log(mName.charAt(7));
+
+const newstring = gameName.substring(0,6)
+const anotherString = gameName.slice(-1,5)
+console.log(anotherString)
