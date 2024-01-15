17b23aa580385ef52e5cea9eb69a62038999d084
Habib Rehman <99414376+Habibr066@users.noreply.github.com>
2024-01-15 07:46:47 +0000

    objects.js working properly

--- basic/object.js
+++ basic/object.js
@@ -1,2 +1,17 @@
 const tinderUser = {}
-console.log(tinderUser)
+
+tinderUser.id = 'userid2233'
+tinderUser.name = 'Hassan Sarddar'
+tinderUser.isLoggin = false
+
+//console.log(tinderUser)
+const regularUser = {
+    email: "habibr066@gmail.com",
+    fullName: {
+        userFullName: {
+            firstName: "Habib",
+            lastName: "Rashidi"
+        }
+    }
+}
+console.log(regularUser)
