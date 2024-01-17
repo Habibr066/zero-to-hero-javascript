50ae8d8874c8b2a3fa0e57ddc41572ffabbab202
Habib Rehman <99414376+Habibr066@users.noreply.github.com>
2024-01-17 09:29:00 +0000

    hek

--- function/fun_js.js
+++ function/fun_js.js
@@ -12,4 +12,24 @@ console.log(number1+number2)
 };
 
 const result = add(4,4)// arguments
-console.log(result);
+//console.log(result);
+
+function calculateCartPrice(...num1){
+    return num1
+}
+
+//console.log(calculateCartPrice(200, 350, 500))
+
+const user = {
+userName: 'habib ur rehman',
+price: 6789,
+}
+
+function handleUser(anyobject){
+console.log('${}')
+
+}
+
+
+    
+handleUser(user)
