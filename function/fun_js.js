f70402aa39c87c1c389694a3dbebf1d1b6af1363
Habib Rehman <99414376+Habibr066@users.noreply.github.com>
2024-01-17 07:45:25 +0000

    function

--- function/fun_js.js
+++ function/fun_js.js
@@ -1,1 +1,15 @@
-console.console.log('hello');
+function sayMyName(){
+    console.log("h");
+    console.log("a");
+    console.log("b");
+    console.log("i");
+}
+//sayMyName()
+
+//working function that add two numbers
+function add(number1,number2){ //parameters
+console.log(number1+number2)
+};
+
+const result = add(4,4)// arguments
+console.log(result);
